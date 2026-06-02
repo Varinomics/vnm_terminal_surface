af2b9ff29980f1b51737111bfcacc9d705a66ddd
Ioannis Makris <imak@imak.gr>
2026-06-02 22:01:16 +0200

    Adopt conservative direct coalescing baseline

--- include/vnm_terminal/internal/stage42_feature_flags.h
+++ include/vnm_terminal/internal/stage42_feature_flags.h
@@ -7,19 +7,20 @@ namespace vnm_terminal::internal {
 
 struct stage42_feature_flags_t
 {
-    bool model_ascii_direct_print                  = true;
-    bool model_ascii_skip_simple_cell_clear        = true;
-    bool snapshot_inline_hyperlink_ids             = true;
-    bool qsg_cached_internal_text_node             = true;
-    bool qsg_trusted_ascii_unchecked_glyphs        = true;
-    bool qsg_text_makeup_single_char_fast_path     = true;
-    bool qsg_ascii_resource_prefilter              = true;
-    bool qsg_group_descriptor_eligibility          = true;
-    bool qsg_monotonic_dirty_probe                 = true;
-    bool render_cell_row_cache                     = true;
+    bool model_ascii_direct_print                   = true;
+    bool model_ascii_skip_simple_cell_clear         = true;
+    bool snapshot_inline_hyperlink_ids              = true;
+    bool qsg_cached_internal_text_node              = true;
+    bool qsg_trusted_ascii_unchecked_glyphs         = true;
+    bool qsg_text_makeup_single_char_fast_path      = true;
+    bool qsg_ascii_resource_prefilter               = true;
+    bool qsg_group_descriptor_eligibility           = true;
+    bool qsg_monotonic_dirty_probe                  = true;
+    bool qsg_descriptor_reuse_frame_key_independent = true;
+    bool render_cell_row_cache                      = true;
 };
 
-inline constexpr std::size_t k_stage42_feature_flag_count = 10U;
+inline constexpr std::size_t k_stage42_feature_flag_count = 11U;
 
 struct stage42_feature_flag_metadata_t
 {
